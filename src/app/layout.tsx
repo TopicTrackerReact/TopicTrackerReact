8e3289cee704717ab5ba6f7c7c7e0b998451b8c5
Nitesh Sunku <nsunku99@gmail.com>
2023-08-05 22:33:49 -0700

    Set up Task Reducer
    Co-authored-by: Christopher Park <ChristopherPark3@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Provider } from 'react-redux'
-import store from './store/store'
+import { Providers } from '@/redux/provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider store={store}>
+        <Providers>
           {children}
-        </Provider>
+        </Providers>
       </body>
     </html>
   )
---
+++ src/redux/slices/taskSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+
+interface Task {
+  id: number,
+  taskName: string,
+  notes: string,
+  isCompleted: boolean,
+}
+
+interface InitialState {
+  taskCache: {
+    [key: string]: Task
+  },
+  totalTasks: number,
+}
+
+const initialState: InitialState = {
+  taskCache: {},
+  totalTasks: 0,
+}
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    createTask(state, action: PayloadAction<Task>) {
+      state.taskCache[action.payload.taskName] = action.payload;
+      state.totalTasks++;
+    },
+    updateTask(state, action: PayloadAction<Task>) {
+      state.taskCache[action.payload.taskName] = action.payload;
+    },
+    updateCompleted(state, action: PayloadAction<string>) {
+      state.taskCache[action.payload].isCompleted === true ? false : true;
+    },
+    deleteTask(state, action: PayloadAction<string>) {
+      delete state.taskCache[action.payload];
+      state.totalTasks--
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { createTask, updateTask, updateCompleted, deleteTask } = taskSlice.actions
+
+export default taskSlice.reducer

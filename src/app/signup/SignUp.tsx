d250a51415bbbc74e9e9df4c62200662cea43fbe
Chris Park <cp3063@columbia.edu>
2023-08-08 15:42:44 -0700

    Begins functionality for sign up component

--- src/app/signup/SignUp.tsx
+++ src/app/signup/SignUp.tsx
@@ -8,19 +8,21 @@ export const SignUp = () => {
 			method: 'POST',
 			headers: {
 				"Content-Type": "application/json"
-			}
+			},
+			body: JSON.stringify({})
 		})
 	}
 	return (
 		<div className="flex flex-col bg-slate-100 w-1/5 h-96 justify-around p-12 drop-shadow-xl">
+			<h1 className="self-center text-lg">Get Started</h1>
 			<label>Email</label>
 			<input className="input h-8 bg-zinc-300 hover:bg-gray-300 hover:scale-105 hover:transition duration-200 drop-shadow focus:outline-none focus:bg-gray-300 focus:scale-105 focus:transition duration-200"/>
-			<div className="flex">
-				<span>
+			<div className="flex justify-between">
+				<span className="flex flex-col">
 					<label>First Name</label>
 					<input className="input h-8 w-32 bg-zinc-300 hover:bg-gray-300 hover:scale-105 hover:transition duration-200 drop-shadow focus:outline-none focus:bg-gray-300 focus:scale-105 focus:transition duration-200"/>
 				</span>
-				<span>
+				<span className="flex flex-col">
 					<label>Last Name </label>
 					<input className="input h-8 w-32 bg-zinc-300 hover:bg-gray-300 hover:scale-105 hover:transition duration-200 drop-shadow focus:outline-none focus:bg-gray-300 focus:scale-105 focus:transition duration-200"/>
 				</span>

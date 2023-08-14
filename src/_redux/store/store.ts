36e0df15fcc40618a21090ad989e7b8cc2e4e652
Nitesh Sunku <nsunku99@gmail.com>
2023-08-14 16:02:09 -0700

    finished end to end testing for taskReducer
    Co-authored-by: Christopher Park <ChristopherPark3@users.noreply.github.com>

--- src/_redux/store/store.ts
+++ src/_redux/store/store.ts
@@ -1,15 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { PreloadedState, configureStore, combineReducers } from '@reduxjs/toolkit';
 import taskReducer from '../features/taskSlice';
 import authReducer from '../features/authSlice';
 
-const store = configureStore({
+const preloadedState = {
+  task: {
+    taskCache: {},
+    taskNames: {},
+    maxId: 0,
+    totalTasks: 0,
+  },
+  auth: {
+    userStatus: false
+  }
+};
+
+const rootReducer = combineReducers({
+  task: taskReducer,
+  auth: authReducer
+});
+
+export const setUpStore = (preloadedState?: PreloadedState<RootState>) => configureStore({
   reducer: {
     task: taskReducer,
     auth: authReducer
-  }
+  },
+  preloadedState
 })
 
+const store = setUpStore();
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+
+// export default store;
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setUpStore>;
+export type AppDispatch = AppStore['dispatch'];
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;

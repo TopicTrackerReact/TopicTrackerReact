3ebbae17d4e2c177bbe6b33380bcaa028cd3567b
Chris Park <cp3063@columbia.edu>
2023-08-16 18:47:34 -0700

    Sets up RTK Query
    Co-authored-by: Nitesh Sunku <nsunku99@gmail.com>

---
+++ src/_redux/features/apiSlice.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const taskApi = createApi({
+  reducerPath: 'taskApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/sql' }),
+  endpoints: (builder) => ({
+    getUserData: builder.query({
+      query: (name) => `/getUser/${email}`,
+    }),
+  }),
+})
+
+// make a call to the next route
+// next route will get from the api, and process the data
+// returned data will be sent to redux
+// and redux hook will just initialize the process
--- src/_redux/provider.tsx
+++ src/_redux/provider.tsx
@@ -2,12 +2,16 @@
 
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { taskApi } from './features/apiSlice';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <Provider store={store}>
-      {children}
+      <ApiProvider api={taskApi}>
+        {children}
+      </ApiProvider>
     </Provider>
   )
 }

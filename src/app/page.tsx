c7f482369b0e400fbe62b81bedf6f8afbedcced8
Chris Park <cp3063@columbia.edu>
2023-08-22 00:14:09 -0700

    Adds flowbite package for light and darkmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import {Input} from './home/input'
+import { Input } from "./home/input";
 
 export default function Landing() {
   return (
-    <div className="h-[40rem] w-screen flex justify-center items-center">
-        <div className="bg-stone-100 h-[38rem] w-[60rem] rounded-lg shadow-2xl border-0 border-stone-100">
-          <Input />
-        </div>
+    <div className="bg-red-100 h-[40rem] w-screen flex justify-between items-center p-24">
+      <div className="bg-orange-900 h-[36rem] w-[44rem] text-[3rem]">
+        <h1>Get started with Timely</h1>
+      </div>
+      <div className="flex flex-col items-center bg-stone-100 h-[38rem] w-[60rem] rounded-lg shadow-2xl border-0 border-stone-100">
+        <Input />
+      </div>
     </div>
-  )
+  );
 }

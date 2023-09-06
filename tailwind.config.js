43089ea6ccc92600da4016d03cff76e0caabdad7
Chris Park <cp3063@columbia.edu>
2023-09-06 01:56:07 -0400

    Changes gradient to top right. Looks much better imo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        light: 'linear-gradient(to right, rgb(216, 180, 254), rgb(250, 250, 250), rgb(250, 250, 250))',
-        dark: 'linear-gradient(to right, rgb(88, 28, 135), rgb(38, 38, 38), rgb(38, 38, 38))'
+        light: 'linear-gradient(to top right, rgb(216, 180, 254), rgb(250, 250, 250), rgb(250, 250, 250))',
+        dark: 'linear-gradient(to top right, rgb(88, 28, 135), rgb(38, 38, 38), rgb(38, 38, 38))'
       },
       fontFamily: {},
       
